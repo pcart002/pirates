24c6f6985b7f686053b43848adc4589e6d0efa06
pcart002 <pcart@none.non>
2024-12-09 19:07:43 -0500

    connecting subs o ech other

--- game/locations/patfile.py
+++ game/locations/patfile.py
@@ -73,6 +73,25 @@ class Sparkle(items.Item):
     def __init__(self):
         super().__init__("Diamonds and Pearls", random.radint(100,300))
 
+class Treasure:
+    display.announce ("You have found Treasure!")
+    def __init__ (self):
+        chance = random.randint(1,100)
+        if chance < 10:
+            self.loot = Bomb
+        elif chance < 30:
+            self.loot = item.Flintlock
+        elif chance < 40:
+            self.loot = item.Cutlass
+        elif chance < 50:
+            self.loot = item.BelayingPin
+        elif chance < 60:
+            config.the_player.ship.food += 1
+        else:
+            self.loot = sparkle
+
+        config.the_player.add_to_inventory([self.loot])
+
 ####################################################################################################
 # Island definition
 ####################################################################################################
@@ -89,6 +108,7 @@ class Island (location.Location):
         self.locations["cave"] = Wet_Cave(self)
         self.locations["reef"] = Reef(self)
         self.locations["bush"] = The_Bush(self)
+        self.locations["tall trees"] = Tall_Trees(self)
 
         self.starting_location = self.locations["beach"]
 
@@ -121,39 +141,116 @@ class Beach_with_ship (location.SubLocation):
         elif (verb == "east"):
             config.the_player.next_loc = self.main_location.locations["cave"]
 
-
-
-
 class Wet_Cave (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "cave"
         self.verbs['north'] = self
-        self.event_chance = 20
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+
+        self.verbs['explore'] = self
+
+        self.item_on_body = Sparkles()
+
+        self.event_chance = 99
         self.events.append(Angry_Crabs())
         self.events.append (drowned_pirates.DrownedPirates())
 
     def enter (self):
-        display.announce ("You and your pirate crew stand at the entrance of a dark, dripping cave. Your ship is at anchor in a small bay north.")
+        display.announce ("You and your pirate crew stand at the entrance of a dark, dripping cave.")
+        if verb == "explore":
+            n = random.radint(1,2)
+            if n != 2
+                display.announce ("You and your crewmates explore the cave. You see the skeleton of an old pirate clutching {self.item_on_body.name}")
 
     def process_verb (self, verb, cmd_list, nouns):
+        if (verb == "east"):
+            display.announce ("You return to the beach.")
+            config.the_player.next_loc = self.main_location.locations["beach"]
+        if (verb == "south" or verb == "west"):
+            display.announce ("You look around the cave and you see...nothing.")
         if (verb == "north"):
-            display.announce ("You return to your ship.")
-            self.main_location.end_visit()
+            display.announce ("You look around the cave and you see a group of drowned pirates.")
+            #Create a new drowned pirate event
+            #Tell the new event to process itself.
+            self.events.append = drowned_pirates.DrownedPirates()
+
+class Tall_Trees (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "tall `trees"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+
+        self.verbs['take'] = self
+        self.item_in_tree = JeweledCutlass() #Treasure from this island
+        self.item_in_clothes = items.Flintlock() #Flintlock from the general item list
+
+        self.event_chance = 50
+        self.events.append(ManEatingMonkeys())
+        self.events.append(ShorePirates())
+
+    def enter (self):
+        edibles = False
+        for e in self.events:
+            if isinstance(e, ManEatingMonkeys):
+                edibles = True
+        description = "You walk into the small forest on the island."
+        if edibles == False:
+             description = description + " Nothing around here looks very edible."
+
+        if self.item_in_tree != None:
+            description = description + f" You see a {self.item_in_tree.name} stuck in a tree."
+        if self.item_in_clothes != None:
+            description = description + f" You see a {self.item_in_clothes.name} in a pile of shredded clothes on the forest floor."
+        display.announce (description)
+
+    def process_verb (self, verb, cmd_list, nouns):
+        if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
+            config.the_player.next_loc = self.main_location.locations["beach"]
+        #Handle taking items. Demo both "take cutlass" and "take all"
+        if verb == "take":
+            if self.item_in_tree == None and self.item_in_clothes == None:
+                display.announce ("You don't see anything to take.")
+            elif len(cmd_list) > 1:
+                at_least_one = False #Track if you pick up an item, print message if not.
+                item = self.item_in_tree
+                if item != None and (cmd_list[1] == item.name or cmd_list[1] == "all"):
+                    display.announce(f"You take the {item.name} from the tree.")
+                    config.the_player.add_to_inventory([item])
+                    self.item_in_tree = None
+                    config.the_player.go = True
+                    at_least_one = True
+                item = self.item_in_clothes
+                if item != None and (cmd_list[1] == item.name or cmd_list[1] == "all"):
+                    display.announce(f"You pick up the {item.name} out of the pile of clothes. ...It looks like someone was eaten here.")
+                    config.the_player.add_to_inventory([item])
+                    self.item_in_clothes = None
+                    config.the_player.go = True
+                    at_least_one = True
+                if at_least_one == False:
+                    display.announce ("You don't see one of those around.")
 
 class The_Bush (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "Bush"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
         self.verbs['east'] = self
+        self.verbs['west'] = self
 
     def enter (self):
-        display.announce ("After docking your ship. You and your pirate crew tunneled your way through the dense bushes, slashing your cutlasses at the tangled vines barring your path. You vanish into the shadows, eyes darting for hidden treasures or lurking foes...")
-
+        display.announce ("After leaving the beach. You and your pirate crew tunneled your way through the dense bushes, slashing your cutlasses at the tangled vines barring your path. You vanish into the shadows, eyes darting for hidden treasures or lurking foes...")
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "east"):
-            display.announce ("You return to your ship")
+            display.announce()
+
             self.main_location.end_visit()
         elif (verb == "north" or verb == "west"):
             display.announce ("You and your crew begin to forage the bush looking for fruit")
@@ -162,22 +259,43 @@ class The_Bush (location.SubLocation):
         elif (verb == 'south'):
             display.announce ("After foraging the bushes, you and your crew encounter a gang of Man Eating Monkeys. Good Luck!")
             self.events.append(ManEatingMonkeys())
+            config.the_player.next_loc = self.main_location.locations["beach"]
 
 class Reef (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "reef"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
         self.verbs['west'] = self
-        self.event_chance = 65
+
+        self.verbs['fish'] = self
+
+        self.event_chance = 70
         self.events.append(Angry_Crabs())
 
     def enter (self):
-        display.announce ("The pirate crew finds themselves floating in the tranquil waters of the coral reef, mes,erized by the beauty surrounding them. Sunlight filters through the crystal-clear waves, creating dancing patterns on the sea floor. Your crew is in awe.... Your ship is anchored west.")
+        display.announce ("The pirate crew finds themselves floating in the tranquil waters of the coral reef, meserized by the beauty surrounding them. Sunlight filters through the crystal-clear waves, creating dancing patterns on the sea floor. Your crew is in awe.... Your ship is anchored on the beach west.")
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "west"):
-            display.announce ("You return to your ship.")
-            self.main_location.end_visit()
+            display.announce ("You return to the beach.")
+            config.the_player.next_loc = self.main_location.locations["beach"]
+        if (verb == "east"):
+            display.announce ("You and your crew find a fishing rod. Would you like to fish?")
+
+            if (verb == "fish")
+                fishing = random.randint(1,7)
+                if fishing = 7:
+                    display.announce ("You caught a fish!")
+                    config.the_player.ship.food += 3
+                else:
+                    display.announce ("Lines dry, maybe the fish arent biting today!")
+
+        if (verb == "north" or verb == "south"):
+            display.announce ("There is only water in the distance!")
+        
 
 class puzzle:
     #def __init__
@@ -205,7 +323,7 @@ class puzzle:
         player_ans = int(input("Whats 🍌 + 🍑 + 🍉 = ? "))
         if player_ans == fruitpunch:
             print('You have found food!')
-            config.the_player.ship.food += fruitpunch*2
+            config.the_player.ship.food += fruitpunch
             return False
         
         else:
@@ -221,20 +339,3 @@ class puzzle:
         play = True
         while play:
             play = self.minigame()
-
-class Treasure:
-    display.announce ("You have found Treasure!")
-    def __init__ (self):
-        chance = random.randint(1,100)
-        if chance < 10:
-            self.loot = Bomb
-        elif chance < 30:
-            self.loot = item.Flintlock
-        elif chance < 40:
-            self.loot = item.Cutlass
-        elif chance < 50:
-            self.loot = item.BelayingPin
-        else:
-            self.loot = sparkle
-
-        config.the_player.add_to_inventory([self.loot])
